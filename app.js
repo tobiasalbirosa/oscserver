9af0bc0b6bc1bd44a6aec2072e955e3747bdb0f6
tobiasalbirosa <tobiasalbirosa@gmail.com>
2021-09-02 15:48:39 -0300

    m

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 'use strict'
 require('dotenv').config()
 var osc = require("osc")
-
 const express = require('express')
 //APP
 const app  = express()
-const PORT =   12000
-const HOST =   process.env.HOST
+const PORT = process.env.PORT
+const HOST = process.env.HOST
 app.listen(PORT)
 const UDPPort = new osc.UDPPort({
   localAdrress : HOST,
   localPort : PORT
 })
+console.log(UDPPort)
 var client = require("socket.io-client");
-var socket = client.connect("https://oscweb.herokuapp.com");
-socket.emit("message", "foo");
+var socket = client.connect("ws://oscweb.herokuapp.com")
+
 UDPPort.open()
 UDPPort.on("message", function (oscMsg, timeTag, info) {
-  console.log("An OSC message just arrived!", oscMsg);
-  console.log("Remote info is: ", info);
+  socket.emit("message", "foo")
 })
   UDPPort.on("ready", function () {
-    console.log(" OSC ready!", HOST, PORT);
+    console.log(" OSC ready!", HOST, PORT)
 })
